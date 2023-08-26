94c4704a5822469c62c95570dc13813e148a63ad
lataba <ltballeste@gmail.com>
2023-08-25 21:14:28 -0700

    Cities view file, change state for city in return, line 59

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -56,7 +56,7 @@ def post_city(state_id):
     data['state_id'] = state_id
     city = City(**data)
     city.save()
-    return jsonify(state.to_dict()), 201
+    return jsonify(city.to_dict()), 201
 
 
 @app_views.route("/cities/<city_id>", methods=["PUT"])
